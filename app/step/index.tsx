084d15744e3ef35a1a3e2884a06a6525b7f0d316
Marcuz015 <marcuzjoaopaulo@gmail.com>
2024-11-06 04:01:03 -0300

    App finalizado

---
+++ app/step/index.tsx
@@ -0,0 +1,126 @@
+import { View, Text, StyleSheet, Pressable, ScrollView } from 'react-native'
+
+import { colors } from '../../constants/Colors'
+import { Header } from '../../components/header'
+import { Input } from '../../components/input'
+
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from  'react-hook-form'
+import { router } from 'expo-router'
+import { useDataStore } from '../../store/data'
+
+
+const squema = z.object({
+  name: z.string().min(1, { message: "O nome é obrigatório"}),
+  weight: z.string().min(1, { message: "O peso é obrigatório"}),
+  age: z.string().min(1, { message: "A idade é obrigatória"}),
+  height: z.string().min(1, { message: "A altura é obrigatória"}),
+})
+
+type FormData = z.infer<typeof squema>
+
+export default function Step(){
+  
+  const { control, handleSubmit, formState: { errors, isValid } } = useForm<FormData>({
+    resolver: zodResolver(squema)
+  })
+
+  const setPageOne = useDataStore(state => state.setPageOne)
+
+
+  function handleCreate(data: FormData){
+    console.log("Está passando os dados da pagina 1 para a 2");
+    
+    setPageOne({
+      name: data.name,
+      weight: data.weight,
+      age: data.age,
+      height: data.height
+    })
+
+    router.push("/create")
+  }
+
+  return(
+    <View style={styles.container}>
+      <Header
+        step='Passo 1'
+        title='Vamos começar'
+      />
+
+      <ScrollView style={styles.content}>
+        <Text style={styles.label}>Nome</Text>
+        <Input
+          name="name"
+          control={control}
+          placeholder="Digite seu nome"
+          error={errors.name?.message}
+          keyboardType="default"
+        />
+        
+        <Text style={styles.label}>Seu peso atual</Text>
+        <Input
+          name="weight"
+          control={control}
+          placeholder="Ex: 60"
+          error={errors.weight?.message}
+          keyboardType="numeric"
+        />     
+        
+        <Text style={styles.label}>Sua altura atual</Text>
+        <Input
+          name="height"
+          control={control}
+          placeholder="Ex: 1.70"
+          error={errors.height?.message}
+          keyboardType="numeric"
+        />     
+        
+        <Text style={styles.label}>Sua idade atual</Text>
+        <Input
+          name="age"
+          control={control}
+          placeholder="Ex: 20"
+          error={errors.age?.message}
+          keyboardType="numeric"
+        />
+
+        <Pressable style={styles.button} onPress={handleSubmit(handleCreate)}>
+          <Text style={styles.buttonText}>Avançar</Text>
+        </Pressable>     
+
+      </ScrollView>
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: colors.background
+  },
+  content:{
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+  label:{
+   fontSize: 16,
+   color: colors.white,
+   fontWeight: 'bold',
+   marginBottom: 8, 
+  },
+  button:{
+    backgroundColor: colors.blue,
+    height: 44,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+  },
+  buttonText:{
+    color: colors.white,
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
+})
---
+++ types/data.ts
@@ -0,0 +1,19 @@
+//Erro corrigido
+
+interface RefeicoesProps{
+  horario: string;
+  nome: string;
+  alimentos: string[];
+}
+
+export interface Data {
+  nome: string;
+  sexo: string;
+  idade: number;
+  altura: number;
+  peso: number;
+  objetivo: number;
+  refeicoes: RefeicoesProps[];
+  suplementos: string[];
+}
+
